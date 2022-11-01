326d393150802b5d490918b3568076a2c3ba48eb
Bakaretw <wilmot.bakare@sterling.ng>
2022-11-01 19:52:53 +0100

    Added validation to UI

--- NigerianBanks.UI/src/app/components/edit-bank/edit-bank.component.ts
+++ NigerianBanks.UI/src/app/components/edit-bank/edit-bank.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Bank } from 'src/app/models/Bank';
 import { BankService } from 'src/app/services/bank.service';
 
+
 @Component({
   selector: 'app-edit-bank',
   templateUrl: './edit-bank.component.html',
@@ -17,20 +18,52 @@ export class EditBankComponent implements OnInit {
   }
 
   updateBank(bank:Bank){
-    this.bankService
-    .updateBanks(bank)
-    .subscribe((banks:Bank[])=> this.banksUpdated.emit(banks));
+    
+    if(this.validateBank(bank)){
+      this.bankService
+      .updateBanks(bank)
+      .subscribe((banks:Bank[])=> this.banksUpdated.emit(banks));
+
+    }
+       alert("Success");
   }
   deleteBank(bank:Bank){
     this.bankService
     .deleteBanks(bank)
     .subscribe((banks:Bank[])=> this.banksUpdated.emit(banks));
+    alert("Success");
   }
   createBank(bank:Bank){
-    this.bankService
-    .createBanks(bank)
-    .subscribe((banks:Bank[])=> this.banksUpdated.emit(banks));
+    if(this.validateBank(bank)){
+      this.bankService
+      .createBanks(bank)
+      .subscribe((banks:Bank[])=> this.banksUpdated.emit(banks));
+    
+    }
+    alert("Success");
   }
+
+  validateBank(bank:Bank){
+
+    let isvalid = true;
+    if (bank.name == "") {
+      alert("Bank Name must be filled out");
+      isvalid = false
+    }
+    else  if (bank.city == "") {
+      alert("Bank city must be filled out");
+      isvalid = false
+    }
+    else  if (bank.slogan == "") {
+      alert("Bank slogan must be filled out");
+      isvalid = false
+    }
+    else  if (bank.code == "") {
+      alert("Bank code must be filled out");
+      isvalid = false
+    }
+    return isvalid;
+    }
 
 
 }
--- NigerianBanks.UI/src/environments/environment.ts
+++ NigerianBanks.UI/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:"https://localhost:7291/api"
+  apiUrl:"https://localhost:62905/api"
 };
 
 /*
