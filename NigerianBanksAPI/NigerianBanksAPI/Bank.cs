f20ed86aa5de5000e6ca0bc78a8d08bd48fbc51d
Bakaretw <wilmot.bakare@sterling.ng>
2022-11-01 01:06:13 +0100

    added bank class and controller

---
+++ NigerianBanksAPI/NigerianBanksAPI/Bank.cs
@@ -0,0 +1,11 @@
+﻿namespace NigerianBanksAPI
+{
+    public class Bank
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Slogan { get; set; } = string.Empty;
+    }
+}
